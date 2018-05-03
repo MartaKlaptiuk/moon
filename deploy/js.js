a40cf83e8f68150a4e55ad2a53648f94f45cf664
Marta Klaptiuk <tusyvfhnf@gmail.com>
2018-05-03 17:05:17 +0300

    123

--- deploy/js.js
+++ deploy/js.js
@@ -34,6 +34,23 @@ moveItItem.prototype.update = function(scrollTop){
   this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
 };
 
+//Scroll
+$(document).ready(function () {
+    $("a").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false; 
+    });
+});
+
+
+
+
 // Initialization
 $(function(){
   $('[data-scroll-speed]').moveIt();
